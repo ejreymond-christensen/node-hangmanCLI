a4bc9ecd0d72caed257a8d1774bfeb0ee35fd721
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-02-19 10:51:55 -0600

    added notes and validated

--- index.js
+++ index.js
@@ -1,70 +1,106 @@
 var word = require("./word.js");
 var inquirer = require("inquirer");
 
-var lives = 5;
+//global variables
+var lives = 10;
 var guessedLetters = [];
 var game = "";
 
-var wordBank = ["nantes france", "brussels belgium", "monaco monaco", "quebec canada", "bern switzerland"];
+//word bank of rugby teams.
+var wordBank = [
+  "ireland",
+  "scotland",
+  "japan - cherry blossoms",
+  "new zealand - all blacks",
+  "south africa - springboks",
+  "italy - azzurri",
+  "england",
+  "france - les bleus",
+  "argentina - los pumas",
+  "united states - eagles",
+  "tonga - sea eagles",
+  "australia - wallabies",
+  "wales",
+  "georgia - lelos",
+  "fiji - flying fijians",
+  "uruguay - los teros"
+];
 
-
+// function to start the game and pick a new word.
 var gameStart = function() {
   guessedLetters = [];
-  randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+  randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
   game = new word.word(randomWord);
   console.log(" ---- New Word ---- ");
   game.returnWord();
   turn();
 };
 
+// function that runs the game logic.
 var turn = function() {
+  // prompts user for input.
   inquirer.prompt({type: "input", message: "Guess a letter!", name: "guess"}).then(function(response) {
+    //validates if input is a letter in alphabet
     if (word.alphabet.indexOf(response.guess.toLowerCase()) === -1) {
       console.log("Please chose a proper letter");
       turn();
-    }else{
-    if (guessedLetters.indexOf(response.guess.toLowerCase()) === -1) {
-      if (game.letters.indexOf(response.guess.toLowerCase()) === -1) {
-        lives = lives - 1;
-        guessedLetters.push(response.guess.toLowerCase());
-        console.log("Sorry, that letter is not in the word");
-        console.log("Current lives:" + lives);
-        if (lives >0) {
-          game.returnWord();
-          turn();
-        }else{
-          console.log("You Loose!");
-          restart();
-        }
-      } else {
-        guessedLetters.push(response.guess.toLowerCase());
-        game.guess(response.guess.toLowerCase());
-        game.returnWord();
-        if (game.currentIteration.indexOf("_ ") === -1) {
-          console.log("You win!");
-          gameStart();
-        }else{
-          turn();
-        }
-      }
     } else {
-      console.log("Sorry, but that letter has already been played");
-      game.returnWord();
-      turn();
+      //validates if the letter has already been chosen (stored in the guessedLetters array)
+      if (guessedLetters.indexOf(response.guess.toLowerCase()) === -1) {
+        //checks if the letter is in the word, if not subtract a life
+        if (game.letters.indexOf(response.guess.toLowerCase()) === -1) {
+          lives = lives - 1;
+          guessedLetters.push(response.guess.toLowerCase());
+          console.log("Sorry, that letter is not in the word");
+          console.log("Current lives:" + lives);
+          //checks how many lives are available, if so next turn
+          if (lives > 0) {
+            game.returnWord();
+            turn();
+            // if not, game over
+          } else {
+            console.log("You Loose!");
+            console.log("");
+            restart();
+          }
+          //if guess passed all validation and is in the word, a good guess is logged, updates the word.
+        } else {
+          guessedLetters.push(response.guess.toLowerCase());
+          console.log("Great Guess!");
+          game.guess(response.guess.toLowerCase());
+          game.returnWord();
+          //if the full word has been guessed (ie, no more underscores), game is won.
+          if (game.currentIteration.indexOf("_ ") === -1) {
+            console.log("You win!");
+            console.log("");
+            gameStart();
+            //if not, keep playing.
+          } else {
+            turn();
+          }
+        }
+        // if player validation fails first if statement (if the letter has already been played), let the player know, retry.
+      } else {
+        console.log("Sorry, but that letter has already been played");
+        game.returnWord();
+        turn();
+      }
     }
-  }
-});
+  });
 };
 
-var restart = function(){
-  inquirer.prompt({type: "confirm", message: "Would you like to play again?", name: "confirm", default: true}).then(function(response){
+//this function restarts the game, via a prompt asking if the play wants to replay.
+var restart = function() {
+  inquirer.prompt({type: "confirm", message: "Would you like to play again?", name: "confirm", default: true}).then(function(response) {
     if (response.confirm === true) {
-      lives = 5;
+      lives = 10;
       gameStart();
-    }else{
+    } else {
       console.log("See you next time!");
     }
   });
 };
 
+//this console logs the start screen and starts the game.
+console.log("\n---==== RUGBY WORLD CUP HANGMAN 2019====---\n");
 gameStart();
--- word.js
+++ word.js
@@ -1,46 +1,73 @@
-//Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
-
-//An array of new Letter objects representing the letters of the underlying word
-
-//A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-
-//A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
-var alphabet =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// brings in letter.js
 var Letter = require("./letter.js");
+// Alphabet array, for input validation and determining non-letters in the word
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "n",
+  "m",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
+// word constructor
 var Word = function(currentWord) {
-  this.letters = currentWord.split("");
-  this.newLettersArray = [];
-  this.currentIteration = "";
-  console.log(this.letters);
+  this.letters = currentWord.split(""); //splits current word into an array.
+  this.newLettersArray = []; // stores blanked word generated from letters.
+  this.currentIteration = ""; // reconstructed word from letters
+
+  // creates the origional word and determines if the letters is alphabetical, thus pushing into Letters. All non-alphabetical letters, will be displayed automatically (ie spaces and -), thus setting boolean guessed to true.
   for (var i = 0; i < this.letters.length; i++) {
-    var character= "";
+    var character = "";
     if (alphabet.indexOf(this.letters[i].toLowerCase()) === -1) {
       character = new Letter(this.letters[i], true);
-    }else{
+    } else {
       character = new Letter(this.letters[i], false);
     }
     this.newLettersArray.push(character);
   }
+  // Constructs the currentIteration so the word can be displayed on to console.
   this.returnWord = function() {
-    var displayWord ="";
+    var displayWord = "";
     for (var i = 0; i < this.newLettersArray.length; i++) {
       displayWord = displayWord + this.newLettersArray[i].guessReturn();
     }
     this.currentIteration = displayWord;
-    console.log(displayWord);
+    console.log("");
+    console.log(displayWord.toUpperCase());
+    console.log("");
   };
+
+  //function to run the guessed letter in the Letter constructor.
   this.guess = function(guessedLetter) {
     for (var i = 0; i < this.newLettersArray.length; i++) {
       this.newLettersArray[i].guessCheck(guessedLetter);
-      if (alphabet.indexOf(guessedLetter) === -1) {
-        Letter.guessed = true;
-      }
     }
   };
 };
 
+//Exports both the constructor and alphabet
 module.exports = {
-	word: Word,
-	alphabet: alphabet
+  word: Word,
+  alphabet: alphabet
 };
